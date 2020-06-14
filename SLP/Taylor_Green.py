ffa8d414a195f4cea31a4395e160c6dc45b68ab7
Jamun-Fanatic-Foreva <prajwal.1305@gmail.com>
2020-06-14 07:17:59 +0530

    UPDATE: ------------- 1. Taylor_Green.py - Add post-processing

--- SLP/Taylor_Green.py
+++ SLP/Taylor_Green.py
@@ -12,11 +12,22 @@ from pysph.sph.integrator import Integrator, IntegratorStep
 
 # Additional import
 import numpy as np
+import os
 pi = np.pi
 
 # Import Delta_Plus - SPH Equations
 from Delta_Plus_SPH import EOS_DeltaPlus_SPH, MomentumEquation_DeltaPlus_SPH, ContinuityEquation_DeltaPlus_SPH
 ##NOTE: git checkout b139a3ba 
+
+
+def exact_solution(U, b, t, x, y):
+    factor = U * np.exp(b*t)
+
+    u = -np.cos(2*pi*x) * np.sin(2*pi*y)
+    v = np.sin(2*pi*x) * np.cos(2*pi*y)
+    p = -0.25 * (np.cos(4*pi*x) + np.cos(4*pi*y))
+
+    return factor * u, factor * v, factor * factor * p
 
 class EulerIntegrator(Integrator):
     def one_timestep(self, t, dt):
@@ -49,27 +60,49 @@ class EulerStep(IntegratorStep):
 
 class Taylor_Green(Application):
     def initialize(self):
-        self.dx = 1.0/50.0
-        self.rho0 = 1000
+        self.nx = 50.0
+        self.re = 100.0
+        self.U = 1.0
+        self.L = 1.0
+        self.rho0 = 1000.0
+
+        self.c0 = 15.0
+        self.hdx = 1.3
+
+        self.nu = self.L * self.U/self.re
+        self.mu = self.nu * self.rho0
+        self.dx = self.L/self.nx
+        
+        
         self.vol = self.dx * self.dx
         self.m0 = self.rho0 * self.vol
-        self.hdx = 1.3
+        
         self.h0 = self.hdx * self.dx
-        self.c0 = 15
+        
+
+        dt_cfl = 0.25 * self.h0 / (self.c0 + self.U)
+        dt_viscous = 0.125 * self.h0**2 / self.nu
+        dt_force = 0.25 * 1.0
+
+        self.dt = min(dt_cfl, dt_viscous, dt_force)
+        self.tf = 2.0
 
     def create_particles(self):
         '''
         Set up particle arrays
         '''
         dx = self.dx
+        L = self.L
         # Fluid
-        x0, y0 = np.mgrid[dx/2: 1-dx/2:dx, dx/2: 1-dx/2:dx]
-        v0 = np.sin(2*pi*x0)*np.cos(2*pi*y0)
-        u0 = -1.0 * np.cos(2*pi*x0)*np.sin(2*pi*y0)
+        x0, y0 = np.mgrid[dx/2: L-dx/2:dx, dx/2: L-dx/2:dx]
+        
+        b = -8.0-pi*pi/self.re
+
+        u0, v0, p0 = exact_solution(U=self.U, b=b, t=0, x=x0, y=y0)
 
 
         pa_fluid = get_particle_array_wcsph(
-            name='fluid', x = x0, y=y0, u=u0, v=v0,  rho = self.rho0, m=self.m0, h=self.h0)
+            name='fluid', x = x0, y=y0, u=u0, v=v0, p=p0, rho = self.rho0, m=self.m0, h=self.h0)
 
         return [pa_fluid]
 
@@ -79,11 +112,8 @@ class Taylor_Green(Application):
         
         integrator = EulerIntegrator(fluid = EulerStep())
 
-        dt = 1e-5
-        tf = 1
-
         solver = Solver(
-            kernel=kernel, dim=2, integrator=integrator, dt=dt, tf=tf, pfreq=50,
+            kernel=kernel, dim=2, integrator=integrator, dt=self.dt, tf=self.tf, pfreq=100,
         )
         return solver
 
@@ -99,7 +129,7 @@ class Taylor_Green(Application):
             Group(
                 equations=[
                     ContinuityEquation_DeltaPlus_SPH(dest='fluid', sources=['fluid'], delta=0.1, c0=self.c0,H=self.h0), 
-                    MomentumEquation_DeltaPlus_SPH(dest='fluid', sources=['fluid'], dim=2, mu=1)
+                    MomentumEquation_DeltaPlus_SPH(dest='fluid', sources=['fluid'], dim=2, mu=self.mu)
                 ],
                     real=True
             )
@@ -107,6 +137,87 @@ class Taylor_Green(Application):
 
         return equations
 
+    # Post processing
+    def _get_post_process_props(self, array):
+        """
+        Return x, y, u, v
+        """
+        x, y, u, v = array.get('x', 'y', 'u', 'v')
+        
+        return x, y, u, v
+
+    def post_process(self, info_fname):
+
+        info = self.read_info(info_fname)
+        if len(self.output_files) == 0:
+            return
+
+        from pysph.solver.utils import iter_output
+        decay_rate = -8.0 * pi**2 / self.re
+
+        files = self.output_files
+        t, ke, ke_ex, decay, linf, l1 = [], [], [], [], [], []
+        for sd, array in iter_output(files, 'fluid'):
+            _t = sd['t']
+            t.append(_t)
+            x, y, u, v = self._get_post_process_props(array)
+            u_e, v_e, p_e = exact_solution(self.U, decay_rate, _t, x, y)
+            vmag2 = u**2 + v**2
+            vmag = np.sqrt(vmag2)
+            ke.append(0.5 * np.sum(self.m0 * vmag2))
+            vmag2_e = u_e**2 + v_e**2
+            vmag_e = np.sqrt(vmag2_e)
+            ke_ex.append(0.5 * np.sum(self.m0 * vmag2_e))
+
+            vmag_max = vmag.max()
+            decay.append(vmag_max)
+            theoretical_max = self.U * np.exp(decay_rate * _t)
+            linf.append(abs((vmag_max - theoretical_max) / theoretical_max))
+
+            l1_err = np.average(np.abs(vmag - vmag_e))
+            avg_vmag_e = np.average(np.abs(vmag_e))
+            # scale the error by the maximum velocity.
+            l1.append(l1_err / avg_vmag_e)
+
+
+        t, ke, ke_ex, decay, l1, linf = list(map(
+            np.asarray, (t, ke, ke_ex, decay, l1, linf))
+        )
+        decay_ex = self.U * np.exp(decay_rate * t)
+        fname = os.path.join(self.output_dir, 'results.npz')
+        np.savez(
+            fname, t=t, ke=ke, ke_ex=ke_ex, decay=decay, linf=linf, l1=l1, decay_ex=decay_ex
+        )
+
+        import matplotlib
+        matplotlib.use('Agg')
+
+        from matplotlib import pyplot as plt
+        plt.clf()
+        plt.semilogy(t, decay_ex, label="exact")
+        plt.semilogy(t, decay, label="computed")
+        plt.xlabel('t')
+        plt.ylabel('max velocity')
+        plt.legend()
+        fig = os.path.join(self.output_dir, "decay.png")
+        plt.savefig(fig, dpi=300)
+
+        plt.clf()
+        plt.plot(t, linf)
+        plt.xlabel('t')
+        plt.ylabel(r'$L_\infty$ error')
+        fig = os.path.join(self.output_dir, "linf_error.png")
+        plt.savefig(fig, dpi=300)
+
+        plt.clf()
+        plt.plot(t, l1, label="error")
+        plt.xlabel('t')
+        plt.ylabel(r'$L_1$ error')
+        fig = os.path.join(self.output_dir, "l1_error.png")
+        plt.savefig(fig, dpi=300)
+
+
 if __name__ == '__main__':
     app = Taylor_Green()
     app.run()
+    app.post_process(app.info_filename)
