c09fe4226fa8305832c00645569b06941b5cb9b0
Jamun-Fanatic-Foreva <prajwal.1305@gmail.com>
2020-06-15 14:15:51 +0530

    UPDATE: ------------- 1. Taylor_Green.py - Corrected periodic boundarys Import PySPH in-built Euler Integrator Imporved  comments and readability

--- SLP/Taylor_Green.py
+++ SLP/Taylor_Green.py
@@ -1,28 +1,43 @@
+###########################################################################
+# IMPORTS
+###########################################################################
+
 # PySPH base imports
 from pysph.base.utils import get_particle_array_wcsph
 from pysph.base.kernels import QuinticSpline
+from pysph.base.nnps import DomainManager
 
-#PySPH solver imports
+# PySPH solver imports
 from pysph.solver.application import Application
 from pysph.solver.solver import Solver
-from pysph.solver.tools import SimpleRemesher
 
-#PySPH sph imports
+# PySPH sph imports
 from pysph.sph.equation import Equation, Group
-from pysph.sph.integrator import Integrator, IntegratorStep
-from pysph.base.nnps import DomainManager
+#from pysph.sph.integrator import Integrator, IntegratorStep
+from pysph.sph.integrator import EulerIntegrator
+from pysph.sph.integrator_step import EulerStep
+
+# PySPH remesher import
+from pysph.solver.tools import SimpleRemesher
 
 # Additional import
 import numpy as np
 import os
+pi = np.pi
+
+# Inculde path
 import sys
 sys.path.insert(1, '/home/prajwal/Desktop/Winter_Project/SLP-Smoothed-Particle-Hydrodynamics')
-pi = np.pi
 
 # Import Delta_Plus - SPH Equations
 from SLP.Delta_Plus_SPH import EOS_DeltaPlus_SPH, MomentumEquation_DeltaPlus_SPH, ContinuityEquation_DeltaPlus_SPH
 ##NOTE: git checkout b139a3ba 
 
+###########################################################################
+# CODE
+###########################################################################
+
+# Remeshing Functions
 def m4p(x=0.0):
     """From the paper by Chaniotis et al. (JCP 2002).
     """
@@ -51,6 +66,7 @@ class M4(Equation):
         yij = abs(XIJ[1]/d_h[d_idx])
         d_prop[d_idx] += m4p(xij)*m4p(yij)*s_temp_prop[s_idx]
 
+# Exact Solution - Taylor Green Vortex
 def exact_solution(U, b, t, x, y):
     factor = U * np.exp(b*t)
 
@@ -60,28 +76,16 @@ def exact_solution(U, b, t, x, y):
 
     return factor * u, factor * v, factor * factor * p
 
-class EulerIntegrator(Integrator):
-    def one_timestep(self, t, dt):
-        self.initialize()
-        self.compute_accelerations()
-        self.stage1()
-        self.do_post_stage(dt,1)
-
-class EulerStep(IntegratorStep):
-    def initialize(self):
-        pass
-
-    def stage1(self, d_idx, d_u, d_v, d_au, d_av, d_x, d_y, d_rho, d_arho, dt=0.0):
-        d_u[d_idx] += dt*d_au[d_idx]
-        d_v[d_idx] += dt*d_av[d_idx]
-       
-        d_x[d_idx] += dt*d_u[d_idx]
-        d_y[d_idx] += dt*d_v[d_idx]
-
-        d_rho[d_idx] += dt*d_arho[d_idx]
-
+###########################################################################
+# Tayloy Green Vortex - Application
+###########################################################################
 class Taylor_Green(Application):
     def initialize(self):
+        '''
+        Initialize simulation paramters
+        '''
+
+        # Simulation Parameters
         self.remesh = 0
 
         self.nx = 50.0
@@ -91,19 +95,19 @@ class Taylor_Green(Application):
         self.rho0 = 1000.0
 
         self.c0 = 15.0
-        self.hdx = 1.2
+        self.hdx = 1.0
 
+        # Calculate simulation parameters
         self.nu = self.L * self.U/self.re
         self.mu = self.nu * self.rho0
         self.dx = self.L/self.nx
         
         
         self.vol = self.dx * self.dx
-        self.m0 = self.rho0 * self.vol
-        
+        self.m0 = self.rho0 * self.vol        
         self.h0 = self.hdx * self.dx
         
-
+        # Simulation time-step parameters
         dt_cfl = 0.25 * self.h0 / (self.c0 + self.U)
         dt_viscous = 0.125 * self.h0**2 / self.nu
         dt_force = 0.25 * 1.0
@@ -111,6 +115,7 @@ class Taylor_Green(Application):
         self.dt = min(dt_cfl, dt_viscous, dt_force)
         self.tf = 2.0
 
+        # Print parameters
         print('dx : ', self.dx)
         print('dt : ', self.dt)
 
@@ -120,7 +125,7 @@ class Taylor_Green(Application):
         '''
         dx = self.dx
         L = self.L
-        _x = np.arange(dx / 2, L, dx)
+        _x = np.arange(dx/2, L, dx)
 
         # Fluid
         x0, y0 = np.meshgrid(_x, _x)
@@ -131,58 +136,84 @@ class Taylor_Green(Application):
 
 
         pa_fluid = get_particle_array_wcsph(
-            name='fluid', x = x0, y=y0, u=u0, v=v0, p=p0, rho = self.rho0, m=self.m0, h=self.h0)
-
+            name='fluid', x = x0, y=y0, u=u0, v=v0, p=p0, rho = self.rho0, 
+            m=self.m0, h=self.h0
+        )
         return [pa_fluid]
 
     def create_domain(self):
-        L1 = self.L + self.dx*2
-        L0 = -self.dx*2
-        return DomainManager(xmin=L0, xmax=L1, ymin=L0, ymax=L1, periodic_in_x=True, periodic_in_y=True)
+        '''
+        Set-up periodic boundary
+        '''
+        L = self.L
+        return DomainManager(
+            xmin=0, xmax=L, ymin=0, ymax=L, 
+            periodic_in_x=True, periodic_in_y=True
+        )
 
-
-    def create_tools(self):        
+    def create_tools(self): 
+        '''
+        Set-up remesher tool
+        '''       
         tools = []
         if self.remesh > 0:
             equations = [M4(dest='interpolate', sources=['fluid'])]
             equations = None
             props = ['u', 'v', 'au', 'av', 'ax', 'ay', 'arho']
 
-            remesher = SimpleRemesher(self, 'fluid', props=props, freq=self.remesh, equations=equations)
+            remesher = SimpleRemesher(
+                self, 'fluid', props=props, freq=self.remesh, 
+                equations=equations
+            )
             tools.append(remesher)
         return tools
 
-
     def create_solver(self):
+        '''
+        Define solver
+        '''
 
         kernel = QuinticSpline(dim=2)
         
         integrator = EulerIntegrator(fluid = EulerStep())
 
-        solver = Solver(kernel=kernel, dim=2, integrator=integrator, dt=self.dt, tf=self.tf, pfreq=10)
+        solver = Solver(
+            kernel=kernel, dim=2, integrator=integrator, dt=self.dt, tf=self.tf, 
+            pfreq=10
+        )
+
         return solver
 
     def create_equations(self):
+        '''
+        Set-up governing equations
+        '''
         equations = [
             Group(
                 equations=[
-                    EOS_DeltaPlus_SPH(dest='fluid', sources=['fluid'],rho0=self.rho0, c0=self.c0)
+                    EOS_DeltaPlus_SPH(
+                        dest='fluid', sources=['fluid'],rho0=self.rho0, 
+                        c0=self.c0
+                    )
                 ], real=False       
             ),
-
     
             Group(
                 equations=[
-                    ContinuityEquation_DeltaPlus_SPH(dest='fluid', sources=['fluid'], delta=0.1, c0=self.c0,H=self.h0), 
-                    MomentumEquation_DeltaPlus_SPH(dest='fluid', sources=['fluid'], dim=2, mu=self.mu)
-                ],
-                    real=True
+                    ContinuityEquation_DeltaPlus_SPH(
+                        dest='fluid', sources=['fluid'], delta=0.1, c0=self.c0, 
+                        H=self.h0
+                    ), 
+                    MomentumEquation_DeltaPlus_SPH(
+                        dest='fluid', sources=['fluid'], dim=2, mu=self.mu
+                    )
+                ], real=True
             )
         ]
 
         return equations
 
-    # Post processing
+    ###### Post processing
     def _get_post_process_props(self, array):
         """
         Return x, y, u, v, m
@@ -231,7 +262,8 @@ class Taylor_Green(Application):
         decay_ex = self.U * np.exp(decay_rate * t)
         fname = os.path.join(self.output_dir, 'results.npz')
         np.savez(
-            fname, t=t, ke=ke, ke_ex=ke_ex, decay=decay, linf=linf, l1=l1, decay_ex=decay_ex
+            fname, t=t, ke=ke, ke_ex=ke_ex, decay=decay, linf=linf, l1=l1, 
+            decay_ex=decay_ex
         )
 
         import matplotlib
@@ -262,6 +294,7 @@ class Taylor_Green(Application):
         plt.savefig(fig, dpi=300)
 
 
+# Main Code
 if __name__ == '__main__':
     app = Taylor_Green()
     app.run()
