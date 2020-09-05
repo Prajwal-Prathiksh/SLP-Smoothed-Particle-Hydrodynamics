0a04d948e204d4d6967ff5791ea8575889784ac8
Jamun-Fanatic-Foreva <prajwal.1305@gmail.com>
2020-09-05 17:22:43 +0530

    UPDATE: -------------- Plotting

--- SLP/Taylor Green Vortex/plot_pysph_testing.py
+++ SLP/Taylor Green Vortex/plot_pysph_testing.py
@@ -1,16 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams.update({'font.size': 20})
+import seaborn as sns
+sns.set_style("ticks", {'axes.facecolor': '#EAEAF2', 'axes.grid': True})
+global savefig_additional 
+global figTitle1
+global figTitle2
 
-
-#nx, perturb = 50, 0.2
-#nx, perturb = str(int(nx)), str(perturb)
 
 file_base = '/home/prajwal/Desktop/Winter_Project/SLP-Smoothed-Particle-Hydrodynamics/SLP/Taylor Green Vortex/PySPH-Testing/'
 
-
-sph_schm = ['08', '03', '16', '17', '14', '18', '19']
-sph_schm = ['14', '18', '19', '17', '08']
+sph_schm = ['14', '18', '19', '00']
 
 sph_schm_legend = {
     '00':  r'$\delta$-SPH', '01': r'$\delta$-SPH, Prt', '02': 'DPSPH-SPH', 
@@ -26,123 +25,66 @@ sph_schm_legend = {
     '19': r'$\delta^+$-SPH, nx=30, Prt, WQK, hdx=1.33',
     '20': 'Check1', '21': 'Check2'
     }
-title_additional = ' | nx = 50 | Perturb = 0.2'
 savefig_additional = ''
-
-################################################################################
-## Plot decay
-################################################################################
-sz = (19.20,10.80)
-plt.figure(figsize=sz)
-
-cnt = 1
-for schm in sph_schm:        
-    file_loc = file_base + '/Outputs/' + schm + '/results.npz'
-    # Read data
-    data = np.load(file_loc)
-    
-    # Unpack
-    t, decay, decay_ex = data['t'], data['decay'], data['decay_ex']
-    
-    # Plot
-
-    if cnt == 1:
-        plt.semilogy(t, decay_ex, '--k', linewidth=3, label="exact") # Exact Solution
-        cnt = 0
-
-    plt.semilogy(t, decay, linewidth=2, label=sph_schm_legend[schm]) # Simulation Solution
-    
-# Formatting    
-plt.xlabel(r't $\rightarrow$')
-plt.ylabel(r'Max Velocity $\rightarrow$')
-tle = r'Max Velocity in flow vs Time' + title_additional
-plt.title(tle)
-plt.legend()
-tle = file_base + '/decay' + savefig_additional + '.png' 
-plt.savefig(tle, dpi=400)
+figTitle1 = 'Taylor-Green Vortex - Error Plot'
+figtitle2 = 'Taylor-Green Vortex - Runtimes'
 
 
-################################################################################
-## Plot L_{\infty} error
-################################################################################
-plt.figure(figsize=sz)
+def custom_plot(sph_schm, sph_schm_legend, sz=(19.2,14.4), save=False):
+    n = len(sph_schm)
+    fig, axs = plt.subplots(2, 2, figsize=sz)
 
-for schm in sph_schm:
-    file_loc = file_base + '/Outputs/' + schm + '/results.npz'
-    # Read data
-    data = np.load(file_loc)
-    
-    # Unpack
-    t, linf = data['t'], data['linf']
-    
-    # Plot
-    plt.plot(t, linf, linewidth=2, label=sph_schm_legend[schm])
-    
-# Formatting
-plt.xlabel(r't $\rightarrow$')
-plt.ylabel(r'$L_\infty$ error $\rightarrow$')
-tle = r'$L_\infty$ error vs Time' + title_additional
-plt.title(tle)
-plt.legend()
-tle = file_base + '/linf_error' + savefig_additional + '.png' 
-plt.savefig(tle, dpi=400)
+    cnt = 0
+    for schm in sph_schm:        
+        file_loc = file_base + '/Outputs/' + schm + '/results.npz'
+        data = np.load(file_loc) # Read data
+        leg = sph_schm_legend[schm]
 
+        t, decay= data['t'], data['decay']
+        
+        axs[0,0].semilogy(t, decay, linewidth=2, label=leg)
 
-################################################################################
-## Plot L_1 error
-################################################################################
-plt.figure(figsize=sz)
+        linf = data['linf']
+        axs[0,1].plot(t, linf, linewidth=2, label=leg)
 
-for schm in sph_schm:
-    file_loc = file_base + '/Outputs/' + schm + '/results.npz'
-    # Read data
-    data = np.load(file_loc)
-    
-    # Unpack
-    t, l1 = data['t'], data['l1']
-    
-    # Plot
-    plt.plot(t, l1,linewidth=2, label=sph_schm_legend[schm])
-    
-# Formatting
-plt.xlabel(r't $\rightarrow$')
-plt.ylabel(r'$L_1$ error $\rightarrow$')
-tle = r'$L_1$ error vs Time' + title_additional
-plt.title(tle)
-plt.legend()
-tle = file_base + '/l1_error' + savefig_additional + '.png' 
-plt.savefig(tle, dpi=400)
+        l1 = data['l1']
+        axs[1,0].plot(t, l1, linewidth=2, label=leg)
 
-################################################################################
-## Plot L_1 error for p
-################################################################################
-plt.figure(figsize=sz)
+        p_l1 = data['p_l1']
+        axs[1,1].plot(t, p_l1, linewidth=2, label=leg)
 
-for schm in sph_schm:
-    file_loc = file_base + '/Outputs/' + schm + '/results.npz'
-    # Read data
-    data = np.load(file_loc)
-    
-    # Unpack
-    t, p_l1 = data['t'], data['p_l1']
-    
-    # Plot
-    plt.plot(t, p_l1,linewidth=2, label=sph_schm_legend[schm])
-    #plt.semilogy(t, p_l1,linewidth=2, label=schm)
-    
-# Formatting
-plt.xlabel(r't $\rightarrow$')
-plt.ylabel(r'$L_1$ error for $p \rightarrow$')
-tle = r'$L_1$ error for $p$ vs Time' + title_additional
-plt.title(tle)
-plt.legend()
-tle = file_base + '/p_l1_error' + savefig_additional + '.png' 
-plt.savefig(tle, dpi=400)
+        cnt += 1
+        if cnt == n:
+            decay_ex =  data['decay_ex']
+            axs[0,0].semilogy(t, decay_ex, '--k', linewidth=3, label="exact")
 
+    # Labels
+    axs[1,0].set_xlabel(r't $\rightarrow$')
+    axs[1,1].set_xlabel(r't $\rightarrow$')
 
-################################################################################
-## Run-time
-################################################################################
+    # Titles
+    fig.suptitle(figTitle1, fontsize=24)
+    axs[0,0].set_title('Max Velocity in flow vs Time', fontsize='large')
+    axs[0,1].set_title(r'$L_\infty$ error vs Time', fontsize='large')
+    axs[1,0].set_title(r'$L_1$ error vs Time', fontsize='large')
+    axs[1,1].set_title(r'$L_1$ error for $p$ vs Time', fontsize='large')
+
+    fig.tight_layout()
+    fig.subplots_adjust(top=0.9, bottom=0.1)
+    lines = []
+    labels = []
+    for ax in fig.axes:
+        axLine, axLabel = ax.get_legend_handles_labels()
+        lines.extend(axLine)
+        labels.extend(axLabel)
+
+    n += 1
+    fig.legend(lines[0:n], labels[0:n], loc = 'lower center', ncol=n, prop={'size': 16})
+
+    if save == True:
+        tle = file_base + '/TGV_error_plot' + savefig_additional + '.png' 
+        fig.savefig(tle, dpi=400)
+
 def extract_RT(file_loc):
 
     from os import walk
@@ -164,52 +106,47 @@ def extract_RT(file_loc):
 
     return rt
 
-
-RT_y = []
-RT_x = []
-for schm in sph_schm:
-    file_loc = file_base + '/Outputs/' + schm 
-    # Read data
-    RT_y.append(extract_RT(file_loc))
-    RT_x.append(sph_schm_legend[schm])
+def run_time_plot(sph_schm, sph_schm_legend, sz=(19.2,14.4), save = False):
     
-# Plotting
-plt.rcParams.update({'font.size': 10})
-sz = (22,10.8)
-fig, ax = plt.subplots(figsize=sz)
+    RT_x, RT_y = [], []
+    for schm in sph_schm:
+        file_loc = file_base + '/Outputs/' + schm 
+        # Read data
+        RT_y.append(extract_RT(file_loc))
+        RT_x.append(sph_schm_legend[schm])
+        
+    # Plotting
+    fig, ax = plt.subplots(figsize=sz)
 
-# Horizontal Bar Plot 
-ax.barh(RT_x, RT_y, height=0.4)
-# Remove axes splines 
-for s in ['top', 'bottom', 'left', 'right']: 
-    ax.spines[s].set_visible(False) 
-  
-# Remove x, y Ticks 
-ax.xaxis.set_ticks_position('none') 
-ax.yaxis.set_ticks_position('none') 
+    # Horizontal Bar Plot 
+    ax.barh(RT_x, RT_y, height=0.4)
+    # Remove axes splines 
+    for s in ['top', 'bottom', 'left', 'right']: 
+        ax.spines[s].set_visible(False) 
+    
+    # Remove x, y Ticks 
+    ax.xaxis.set_ticks_position('none') 
+    ax.yaxis.set_ticks_position('none') 
 
-ax.set_xlabel(r'Run time (second) $\rightarrow$')
-  
-# Add padding between axes and labels 
-#ax.xaxis.set_tick_params(pad = 5) 
-#ax.yaxis.set_tick_params(pad = 10) 
-  
-# Add x, y gridlines 
-ax.grid(b = True, color ='black', 
-        linestyle ='-.', linewidth = 0.7, 
-        alpha = 0.2) 
-  
-# Show top values  
-#ax.invert_yaxis() 
-  
-# Add annotation to bars 
-for i in ax.patches: 
-    plt.text(i.get_width(), i.get_y()+0.2,  
-             str(round((i.get_width()), 2)), 
-             fontsize = 11, fontweight ='bold', 
-             color ='grey') 
-  
-# Add Plot Title 
-ax.set_title('Run Time - SPH Schemes') 
-tle = file_base + '/run_time' + savefig_additional + '.png' 
-plt.savefig(tle, dpi=400)
+    ax.set_xlabel(r'Run time (second) $\rightarrow$')
+    
+    # Add x, y gridlines 
+    ax.grid(b = True, color ='black', linestyle ='-.', linewidth = 0.7, alpha = 0.2) 
+    
+    # Add annotation to bars 
+    for i in ax.patches: 
+        plt.text(i.get_width(), i.get_y()+0.2,  
+                str(round((i.get_width()), 2)), 
+                fontsize = 11, fontweight ='bold', 
+                color ='grey') 
+    
+    # Add Plot Title 
+    ax.set_title(figtitle2, fontsize=24) 
+    if save == True:
+        tle = file_base + '/TGV_runtime' + savefig_additional + '.png' 
+        fig.savefig(tle, dpi=400)
+
+
+custom_plot(sph_schm, sph_schm_legend, save=True)
+run_time_plot(sph_schm, sph_schm_legend, save=True)
+
